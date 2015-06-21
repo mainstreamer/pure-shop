e0d484eab6215b5b60aafb61db9c0e237a11e71a
Mainstreamer <artem.kyiv@outlook.com>
2015-06-21 22:37:01 +0300

    added categories and items, mocked database

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: meraxes
+ * Date: 6/21/15
+ * Time: 8:02 PM
+ */
+
+$categories = ['electronics' => 1, 'shoes' => 2, 'services' => 3];
+$items= [1 => ['iphone','ipad','macbook'], 2=> ['nike','adidas','puma'], 3=> ['legal','it','cooking']];
--- src/controllers/main.php
+++ src/controllers/main.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once './app/twig/lib/Twig/Autoloader.php';
+require_once 'app/twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
 try {
@@ -17,8 +17,19 @@ $twig = new Twig_Environment($loader);
 // render template
 //echo $template->render([
 
-    if ($_GET['page']=='catalogue') {
-        echo $twig->render('catalogue.twig',$_GET);
+    if ($_GET['page']=='catalogue' && isset ($_GET['category'])) {
+        include 'db.php'; //database mock
+        $category_id = $categories[$_GET['category']];
+        $input = ['x'=>$categories,'get'=>$_GET, 'items'=>$items, 'cid'=>$category_id];
+
+        echo $twig->render('category.twig',$input);
+    }
+
+    elseif ($_GET['page']=='catalogue') {
+     include 'db.php'; //database mock
+
+        $input = ['x'=>$categories,'get'=>$_GET];
+        echo $twig->render('catalogue.twig',/*array_merge($_GET,$categories)*/ $input);
     } else
 
 echo $twig->render('index.twig',[
